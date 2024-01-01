2986972fa5fcf05025683357c0e41b65331f4e7d
Mary McDonnell <g00401878@atu.ie>
2024-01-01 17:20:15 +0000

    Project Done

--- MYSQLDAO.js
+++ MYSQLDAO.js
@@ -1,7 +1,7 @@
 var pmysql = require('promise-mysql')
 
 var pool;
-
+//connect to the mysql database
 pmysql.createPool({
     connectionLimit: 3,
     host: 'localhost',
@@ -16,7 +16,7 @@ pmysql.createPool({
         console.log("pool error:" + e)
     })
 
-
+//using a promise get all the data from the store table using a sql query from the labs
 var getStores = function () {
     return new Promise((resolve, reject) => {
         pool.query('SELECT * FROM store')
@@ -28,10 +28,12 @@ var getStores = function () {
             })
     })
 }
-
+//make the products table by using a left join (from class) to make it look like the picture in the spec
+//the table should display all the nessasary details even if the product is not sold in a store
 var getProducts = function () {
     return new Promise((resolve, reject) => {
-        pool.query('SELECT p.pid, p.productdesc, ps.sid, s.location, ps.price FROM product p LEFT JOIN product_store ps ON p.pid = ps.pid LEFT JOIN store s ON ps.sid = s.sid')
+        //mysql query to create the joined table
+        pool.query('SELECT p.pid, p.productdesc, pst.sid, s.location, pst.price FROM product p LEFT JOIN product_store pst ON p.pid = pst.pid LEFT JOIN store s ON pst.sid = s.sid')
             .then((data) => {
                 resolve(data)
             })
@@ -40,13 +42,13 @@ var getProducts = function () {
             })
     })
 }
-
+//
 var deleteProducts = function (pid) {
     
     return new Promise((resolve, reject) => {
     var myQuery = {
         //https://www.w3schools.com/mysql/mysql_exists.asp
-        sql: 'DELETE FROM product WHERE pid=? AND NOT EXISTS (SELECT 1 FROM product_store WHERE pid = ?)',
+        sql: 'DELETE FROM product WHERE pid=? AND NOT EXISTS (SELECT 1 FROM product_store WHERE pid =?)',
         values: [pid, pid]
     }
     pool.query(myQuery)
@@ -60,13 +62,15 @@ var deleteProducts = function (pid) {
 }
 
 
-//https://www.w3schools.com/mysql/mysql_update.asp
+//function to update the table with new information but not the sid
 var editStores = function (sid, location, mgrid) {
     
     return new Promise((resolve, reject) => {
     var myQuery = {
+        //https://www.w3schools.com/mysql/mysql_update.asp
+        //dont change sid
         sql: 'UPDATE store SET location= ?, mgrid = ? WHERE sid = ?',
-        values: [location, mgrid, sid]
+        values: [location, mgrid, sid]//in order used
     }
     pool.query(myQuery)
         .then((data) => {
@@ -77,11 +81,13 @@ var editStores = function (sid, location, mgrid) {
         })
     })
 }
-
+//function to add the store into the table - manager should nt be used twice in this either
+//INSERT INTO store (sid, location, mgrid) VALUES (?, ?, ?) WHERE NOT EXISTS (SELECT 1 FROM store WHERE mgrid = ?) tried doing this but stopped the program
 var addStore = function (sid, location, mgrid) {
     console.log(sid)
     return new Promise((resolve, reject) => {
     var myQuery = {
+        //https://www.w3schools.com/mysql/mysql_insert.asp
         sql: 'INSERT INTO store (sid, location, mgrid) VALUES (?, ?, ?)',
         values: [sid, location, mgrid]
     }
@@ -95,5 +101,5 @@ var addStore = function (sid, location, mgrid) {
     })
 }
     
-
+//export to index.js to be used
  module.exports = { getStores, addStore, editStores, getProducts, deleteProducts}
--- index.js
+++ index.js
@@ -29,8 +29,7 @@ app.get('/stores', (req, res) => {
 //route to add a new row in the table
 app.get('/stores/addStore', (req, res) => {
 
-    // res.send(employees)
-    res.render("addStore", { errors: undefined })//load the html page
+    res.render("addStore", { errors: undefined })//load the ejs page
 })
 //validate new data and iff it is all ok, add to the table
 app.post('/stores/addStore', [
@@ -42,38 +41,44 @@ app.post('/stores/addStore', [
     console.log(req.body.sid)
     console.log(req.body.location)
     console.log(req.body.mgrid)
-
+    //if there is errors display what error to the top of the addStore page in a list
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         console.log(errors)
     res.render("addStore",
     {errors:errors.errors})
-    } else {
+    } 
+    //if there is not errors add the store to the table
+    else {
         mySQLDAO.addStore(req.body.sid, req.body.location, req.body.mgrid)
         .then((data) => {
             console.log(data)
             console.log("added")
-            res.redirect('/stores')
+            res.redirect('/stores')//send back to the store page
 
         })
+        //catch the error that you cant have 2 managers managing the same store
         .catch((error) => {
-            console.log("y")
             // Handle error
-            //if(error.code == )//look up error.code for specific error - other than that just print error
-            res.render("errors", { "myerror": "cant add" })
+            //res.render("errors", { "myerror": "cant add - Manager is already used" })
+            //display it at the top of the page instead of a seperate error page
+            res.render("addStore", { errors: [{ msg: "Error: Manager ID " + req.body.mgrid + " already exists"}] })
         })
     }  
 })
-
+//edit the store with that specific sid
 app.get('/stores/editStore/:sid', (req, res) => {
 
-    // res.send(employees)
     res.render("editStore", { errors: undefined })//load the html page
 })
 
+//to edit a store - must meet requirements such not being able to edit sid
+//i didnt put validation for updating the sid because it wont let me update the sid anyway because i passed it into the route
 app.post('/stores/editStore/:sid', [
+    //location must be 1 char in length at least
     check("location").isLength({ min: 1 })
         .withMessage("Location must be at least 1 character"),
+    //manager id must be 4 chars
     check("mgrid").isLength({ min: 4, max:4 })
         .withMessage("Manager ID must be 4 chars long")
 ], (req, res) => {
@@ -85,8 +90,10 @@ app.post('/stores/editStore/:sid', [
     if (!errors.isEmpty()) {
         console.log(errors)
     res.render("editStore",
-    {errors:errors.errors})
-    } else {
+    {errors:errors.errors})//load errors to top of page
+    } 
+    //if there is not errors update the store
+    else {
         mySQLDAO.editStores(req.body.sid, req.body.location, req.body.mgrid)
         .then((data) => {
             console.log(data)
@@ -95,7 +102,6 @@ app.post('/stores/editStore/:sid', [
 
         })
         .catch((error) => {
-            console.log("y")
             // Handle error
             res.render("errors", { "myerror": "cant edit" })
         })
@@ -103,10 +109,11 @@ app.post('/stores/editStore/:sid', [
 })
 
 ////////////Products Page///////////////////////////////
+//display the products page using a left join
 app.get('/products', (req, res) => {
     mySQLDAO.getProducts()
         .then((data) => {
-            res.render("products", { "products": data })
+            res.render("products", { "products": data })//load the table
         })
         .catch((error) => {
             res.send(error)
@@ -114,17 +121,29 @@ app.get('/products', (req, res) => {
 
 })
 
+//delete a product if not sold in a store
 app.get('/products/delete/:pid', (req, res) => {
+    //call the delete functon
     mySQLDAO.deleteProducts(req.params.pid)
         .then((data) => {console.log(data)
-            console.log("deleted "+ req.params.pid)
-            res.redirect('/products')
+            //It wouldnt delete if its not supposed to but
+            //I couldnt get the error message to display so I googled how to show it
+            //https://www.w3schools.com/nodejs/nodejs_mysql_delete.asp
+            //found out affectedRows being zero means nothing was changed so to show the message if it doesnt delete
+            if(data.affectedRows == 0){
+                //display error message on seperate page
+                res.render("errors", {"myerror": req.params.pid + " is currently in stores and cannot be deleted"})
+            }
+            else{
+                console.log("deleted "+ req.params.pid)
+                 res.redirect('/products')
+            
+            }
             
         })
         .catch((error) => {
             console.log("y")
             // Handle error
-            //if(error.code == )//look up error.code for specific error - other than that just print error
             res.render("errors", {"myerror": req.params.pid + " is currently in stores and cannot be deleted"})
         })
  })
@@ -135,7 +154,7 @@ app.get('/products/delete/:pid', (req, res) => {
 app.get('/managers', (req, res) => {
     mongoDAO.findAllManagers()
         .then((data) => {
-            res.render("manager", { "managers": data })
+            res.render("manager", { "managers": data })//load table in ejs page
         })
         .catch((error) => {
 
@@ -143,43 +162,52 @@ app.get('/managers', (req, res) => {
 
 })
 //get the page to add a new manager
-app.get('/addManager', (req, res) => {
+app.get('/managers/addManager', (req, res) => {
 
     res.render("addManager", { errors: undefined })//load the html page
 })
 
 //post method - to add the manager to the database
-app.post('/addManager', [
+app.post('/managers/addManager', [
     //using validation from lab 7 - if the requirements are not met display error message
+    //make id be 4 chars in length
     check("id").isLength({ min: 4, max: 4 })
         .withMessage("ID has to be 4 characters long"),
+    //make name be at least 5 chars
     check("name").isLength({ min: 5 })
         .withMessage("Name must be > 5 characters long"),
+    //make the salary be between 30000 - 70000
     check("salary").isFloat({ min: 30000, max: 70000 })
         .withMessage("Salary must be between 30000 and 70000")
 ], (req, res) => {
+    //send to console for testing
     console.log(req.body.id)
     console.log(req.body.name)
     console.log(req.body.salary)
 
+    //if there is errors reload the html and display the error messages in bullet points at the top of the screen
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         console.log(errors)
     res.render("addManager",
     {errors:errors.errors})
-    } else {
-        
+    } 
+    //else if there is no errors add the new manger to the db
+    else {
+        //call method
         mongoDAO.addManager({
-
+            //get the attributes from the input
             _id: req.body.id,
             name: req.body.name,
             salary: req.body.salary
         })
             .then((result) => {
+                //reload the managers page with the update
                 res.redirect('/managers')
             })
             .catch((error) => {
                 console.log(error)
+                //using the specific error code for a not unique id - print out the message at the top of the screen
                 if (error.code == 11000) {
                     res.render("addManager", { errors: [{ msg: "Error: Manager ID " + req.body.id + " already exists"}] })
                 }
